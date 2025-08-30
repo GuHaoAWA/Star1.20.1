820adcdf27d4cd7cdaf6801fe496136fde780b04
Arcadia <1919463065@qq.com>
2025-08-30 16:01:19 +0800

    2012

--- src/main/java/com/guhao/stars/mixins/epicfight/DodgeAnimationMixin.java
+++ src/main/java/com/guhao/stars/mixins/epicfight/DodgeAnimationMixin.java
@@ -1,4 +1,3 @@
-/*
 package com.guhao.stars.mixins.epicfight;
 
 import com.guhao.stars.units.StarDataUnit;
@@ -10,33 +9,58 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import yesman.epicfight.api.animation.AnimationManager.AnimationAccessor;
 import yesman.epicfight.api.animation.types.ActionAnimation;
 import yesman.epicfight.api.animation.types.DodgeAnimation;
 import yesman.epicfight.api.animation.types.EntityState;
+import yesman.epicfight.api.asset.AssetAccessor;
 import yesman.epicfight.api.model.Armature;
 import yesman.epicfight.api.utils.AttackResult;
 import yesman.epicfight.world.damagesource.EpicFightDamageTypeTags;
 
 import java.util.function.Function;
 
-@Mixin(value = DodgeAnimation.class,remap = false)
+@Mixin(value = DodgeAnimation.class, remap = false)
 public abstract class DodgeAnimationMixin extends ActionAnimation {
-    public DodgeAnimationMixin(float convertTime, String path, Armature armature) {
-        super(convertTime, path, armature);
+    public DodgeAnimationMixin(float convertTime, AnimationAccessor<? extends ActionAnimation> accessor, AssetAccessor<? extends Armature> armature) {
+        super(convertTime, accessor, armature);
     }
+
     @Unique
     private static final Function<DamageSource, AttackResult.ResultType> star$DODGEABLE_SOURCE_VALIDATOR = (damagesource) -> {
         if (StarDataUnit.getEpicFightDamageSources(damagesource) != null) {
-            return damagesource.getEntity() != null && !damagesource.is(DamageTypeTags.IS_EXPLOSION) && !StarDataUnit.isNoDodge(StarDataUnit.getEpicFightDamageSources(damagesource).getAnimation().get()) && !damagesource.is(DamageTypes.MAGIC) && !damagesource.is(DamageTypeTags.BYPASSES_ARMOR) && !damagesource.is(DamageTypeTags.BYPASSES_INVULNERABILITY) && !damagesource.is(EpicFightDamageTypeTags.BYPASS_DODGE) ? AttackResult.ResultType.MISSED : AttackResult.ResultType.SUCCESS;
+            return damagesource.getEntity() != null &&
+                    !damagesource.is(DamageTypeTags.IS_EXPLOSION) &&
+                    !StarDataUnit.isNoDodge(StarDataUnit.getEpicFightDamageSources(damagesource).getAnimation().get()) &&
+                    !damagesource.is(DamageTypes.MAGIC) &&
+                    !damagesource.is(DamageTypeTags.BYPASSES_ARMOR) &&
+                    !damagesource.is(DamageTypeTags.BYPASSES_INVULNERABILITY) &&
+                    !damagesource.is(EpicFightDamageTypeTags.BYPASS_DODGE) ?
+                    AttackResult.ResultType.MISSED : AttackResult.ResultType.SUCCESS;
         }
-        return damagesource.getEntity() != null && !damagesource.is(DamageTypeTags.IS_EXPLOSION) && !damagesource.is(DamageTypes.MAGIC) && !damagesource.is(DamageTypeTags.BYPASSES_ARMOR) && !damagesource.is(DamageTypeTags.BYPASSES_INVULNERABILITY) && !damagesource.is(EpicFightDamageTypeTags.BYPASS_DODGE) ? AttackResult.ResultType.MISSED : AttackResult.ResultType.SUCCESS;
+        return damagesource.getEntity() != null &&
+                !damagesource.is(DamageTypeTags.IS_EXPLOSION) &&
+                !damagesource.is(DamageTypes.MAGIC) &&
+                !damagesource.is(DamageTypeTags.BYPASSES_ARMOR) &&
+                !damagesource.is(DamageTypeTags.BYPASSES_INVULNERABILITY) &&
+                !damagesource.is(EpicFightDamageTypeTags.BYPASS_DODGE) ?
+                AttackResult.ResultType.MISSED : AttackResult.ResultType.SUCCESS;
     };
 
-
-
-    @Inject(method = "<init>(FFLjava/lang/String;FFLyesman/epicfight/api/model/Armature;)V",at = @At("TAIL"))
-    public void setStar$DODGEABLE_SOURCE_VALIDATOR(float convertTime, float delayTime, String path, float width, float height, Armature armature, CallbackInfo ci) {
-        this.stateSpectrumBlueprint.clear().newTimePair(0.0F, delayTime).addState(EntityState.TURNING_LOCKED, true).addState(EntityState.MOVEMENT_LOCKED, true).addState(EntityState.UPDATE_LIVING_MOTION, false).addState(EntityState.CAN_BASIC_ATTACK, false).addState(EntityState.CAN_SKILL_EXECUTION, false).addState(EntityState.INACTION, true).newTimePair(0.0F, Float.MAX_VALUE).addState(EntityState.ATTACK_RESULT, star$DODGEABLE_SOURCE_VALIDATOR);
+    @Inject(method = "<init>(FFLyesman/epicfight/api/animation/AnimationManager$AnimationAccessor;FFLyesman/epicfight/api/asset/AssetAccessor;)V",
+            at = @At("TAIL"))
+    public void setStar$DODGEABLE_SOURCE_VALIDATOR(float transitionTime, float delayTime,
+                                                   AnimationAccessor<? extends DodgeAnimation> accessor, float width, float height,
+                                                   AssetAccessor<? extends Armature> armature, CallbackInfo ci) {
+        this.stateSpectrumBlueprint.clear()
+                .newTimePair(0.0F, delayTime)
+                .addState(EntityState.TURNING_LOCKED, true)
+                .addState(EntityState.MOVEMENT_LOCKED, true)
+                .addState(EntityState.UPDATE_LIVING_MOTION, false)
+                .addState(EntityState.CAN_BASIC_ATTACK, false)
+                .addState(EntityState.CAN_SKILL_EXECUTION, false)
+                .addState(EntityState.INACTION, true)
+                .newTimePair(0.0F, Float.MAX_VALUE)
+                .addState(EntityState.ATTACK_RESULT, star$DODGEABLE_SOURCE_VALIDATOR);
     }
 }
-*/
